4cc9c4ffe0779de7616963a0f16e9a71fab08b7d
htrgouvea <hi@heitorgouvea.me>
2023-02-06 22:16:49 -0300

    start the classes

---
+++ lib/Zarn/AST.pm
@@ -0,0 +1,17 @@
+package Zarn::AST {
+    use strict;
+    use warnings;
+    use PPI::Document;
+
+    sub main {
+        my ($self) = @_;
+
+        if () {
+
+        }
+
+        return 1;
+    }
+}
+
+1;
---
+++ lib/Zarn/Files.pm
@@ -0,0 +1,29 @@
+package Zarn::Files {
+    use strict;
+    use warnings;
+    use File::Find::Rule;
+
+    sub new {
+        my ($self, $source, $ignore) = @_;
+
+        if ($source) {
+            my $rule = File::Find::Rule -> new();
+
+            $rule -> or (
+                $rule -> new -> directory -> name(".git", $ignore) -> prune -> discard,
+                $rule -> new
+            );
+
+            $rule -> file() -> nonempty();
+            $rule -> name("*.pm", "*.t", "*.pl");
+
+            my @files = $rule -> in($source);
+
+            return @files;
+        }
+
+        return 1;
+    }
+}
+
+1;
