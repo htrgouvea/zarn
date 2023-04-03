5ba763c7e47cabf606885a00d99be1e71029ea4b
andersonbosa <andersonbosa0@gmail.com>
2023-04-03 12:14:59 -0300

    * refactor(Zarn::AST.pm): extract matching and processing logic into separate methods * feat(Zarn::AST.pm): add validation for required parameters 'file' and 'rules'

--- lib/Zarn/AST.pm
+++ lib/Zarn/AST.pm
@@ -14,44 +14,62 @@ package Zarn::AST {
             "file=s"  => \$file,
             "rules=s" => \$rules
         );
+        
+        if (!$file && !$rules) {
+            warn "Both file and rules parameters are required.";
+            return;
+        }
 
-        if ($file && $rules) {
-            my $document = PPI::Document -> new($file);
-            
-            $document -> prune("PPI::Token::Pod");
-            $document -> prune("PPI::Token::Comment");
+        my $document = PPI::Document->new($file);
+        $document->prune("PPI::Token::Pod");
+        $document->prune("PPI::Token::Comment");
 
-            foreach my $token (@{$document -> find("PPI::Token")}) {                    
-                foreach my $rule (@{$rules}) {
-                    my @sample   = $rule -> {sample} -> @*;
-                    my $category = $rule -> {category};
-                    my $title    = $rule -> {name};
+        foreach my $token (@{$document->find("PPI::Token")}) {                    
+            foreach my $rule (@{$rules}) {
+                my @sample   = $rule->{sample}->@*;
+                my $category = $rule->{category};
+                my $title    = $rule->{name};
 
-                    if (grep {my $content = $_; scalar(grep {$content =~ m/$_/} @sample)} $token -> content()) {
-                        my $next_element = $token -> snext_sibling;
+                if ($self->_matches_sample($token->content(), \@sample)) {
+                    $self->_process_sample_match($document, $category, $file, $title, $token);
+                }
+            }       
+        }
+    }
 
-                        # this is a draft source-to-sink function
-                        if (defined $next_element && ref $next_element && $next_element -> content() =~ /[\$\@\%](\w+)/) {  
-                            # perform taint analyis
-                            my $var_token = $document -> find_first(
-                                sub { $_[1] -> isa("PPI::Token::Symbol") and $_[1] -> content eq "\$$1" }
-                            );
+    sub _matches_sample {
+        my ($self, $content, $sample) = @_;
 
-                            if ($var_token && $var_token -> can("parent")) {
-                                if ((
-                                    $var_token -> parent -> isa("PPI::Token::Operator") ||
-                                    $var_token -> parent -> isa("PPI::Statement::Expression")
-                                )) {                                    
-                                    print "[$category] - FILE:$file \t Potential: $title.\n";
-                                }
-                            }
-                        }
-                    }
-                }       
+        return grep {
+            my $sample_content = $_;
+            scalar(grep {$content =~ m/$_/} @$sample)
+        } @$sample;
+    }
+
+    sub _process_sample_match {
+        my ($self, $document, $category, $file, $title, $token) = @_;
+
+        my $next_element = $token->snext_sibling;
+
+        # this is a draft source-to-sink function
+        if (defined $next_element && ref $next_element && $next_element->content() =~ /[\$\@\%](\w+)/) {  
+            # perform taint analysis
+            $self->_perform_taint_analysis($document, $category, $file, $title, $next_element);
+        }
+    }
+
+    sub _perform_taint_analysis {
+        my ($self, $document, $category, $file, $title, $next_element) = @_;
+
+        my $var_token = $document->find_first(
+            sub { $_[1]->isa("PPI::Token::Symbol") and $_[1]->content eq "\$$1" }
+        );
+
+        if ($var_token && $var_token->can("parent")) {
+            if (($var_token->parent->isa("PPI::Token::Operator") || $var_token->parent->isa("PPI::Statement::Expression"))) {                                    
+                print "[$category] - FILE:$file \t Potential: $title.\n";
             }
         }
-        
-        return 1;
     }
 }
 
