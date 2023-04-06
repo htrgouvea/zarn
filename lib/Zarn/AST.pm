06acd58ecd25b28be82dbf66c5d002c80e47a09b
htrgouvea <hi@heitorgouvea.me>
2023-04-06 07:26:55 -0300

    fix code style

--- lib/Zarn/AST.pm
+++ lib/Zarn/AST.pm
@@ -20,24 +20,25 @@ package Zarn::AST {
             return;
         }
 
-        my $document = PPI::Document->new($file);
-        $document->prune("PPI::Token::Pod");
-        $document->prune("PPI::Token::Comment");
+        my $document = PPI::Document -> new($file);
 
-        foreach my $token (@{$document->find("PPI::Token")}) {                    
+        $document -> prune("PPI::Token::Pod");
+        $document -> prune("PPI::Token::Comment");
+
+        foreach my $token (@{$document -> find("PPI::Token")}) {                    
             foreach my $rule (@{$rules}) {
-                my @sample   = $rule->{sample}->@*;
-                my $category = $rule->{category};
-                my $title    = $rule->{name};
+                my @sample   = $rule -> {sample} -> @*;
+                my $category = $rule -> {category};
+                my $title    = $rule -> {name};
 
-                if ($self->_matches_sample($token->content(), \@sample)) {
-                    $self->_process_sample_match($document, $category, $file, $title, $token);
+                if ($self -> matches_sample($token -> content(), \@sample)) {
+                    $self -> process_sample_match($document, $category, $file, $title, $token);
                 }
             }       
         }
     }
 
-    sub _matches_sample {
+    sub matches_sample {
         my ($self, $content, $sample) = @_;
 
         return grep {
@@ -46,27 +47,27 @@ package Zarn::AST {
         } @$sample;
     }
 
-    sub _process_sample_match {
+    sub process_sample_match {
         my ($self, $document, $category, $file, $title, $token) = @_;
 
-        my $next_element = $token->snext_sibling;
+        my $next_element = $token -> snext_sibling;
 
         # this is a draft source-to-sink function
-        if (defined $next_element && ref $next_element && $next_element->content() =~ /[\$\@\%](\w+)/) {  
+        if (defined $next_element && ref $next_element && $next_element -> content() =~ /[\$\@\%](\w+)/) {  
             # perform taint analysis
-            $self->_perform_taint_analysis($document, $category, $file, $title, $next_element);
+            $self -> perform_taint_analysis($document, $category, $file, $title, $next_element);
         }
     }
 
-    sub _perform_taint_analysis {
+    sub perform_taint_analysis {
         my ($self, $document, $category, $file, $title, $next_element) = @_;
 
-        my $var_token = $document->find_first(
-            sub { $_[1]->isa("PPI::Token::Symbol") and $_[1]->content eq "\$$1" }
+        my $var_token = $document -> find_first(
+            sub { $_[1 ] -> isa("PPI::Token::Symbol") and $_[1] -> content eq "\$$1" }
         );
 
-        if ($var_token && $var_token->can("parent")) {
-            if (($var_token->parent->isa("PPI::Token::Operator") || $var_token->parent->isa("PPI::Statement::Expression"))) {                                    
+        if ($var_token && $var_token -> can("parent")) {
+            if (($var_token -> parent -> isa("PPI::Token::Operator") || $var_token -> parent -> isa("PPI::Statement::Expression"))) {                                    
                 print "[$category] - FILE:$file \t Potential: $title.\n";
             }
         }
--- lib/Zarn/Files.pm
+++ lib/Zarn/Files.pm
@@ -6,23 +6,22 @@ package Zarn::Files {
     sub new {
         my ($self, $source, $ignore) = @_;
 
-        # Check if source directory is provided
         unless ($source) {
             warn "Source directory not provided";
-            return 0; # REVIEW o 0 é para retornar sucesso?
+            return 0;
         }
 
-        my $rule = File::Find::Rule->new();
+        my $rule = File::Find::Rule -> new();
 
-        $rule->or(
-            $rule->new->directory->name(".git", $ignore)->prune->discard,
-            $rule->new
+        $rule -> or (
+            $rule -> new -> directory -> name(".git", $ignore) -> prune -> discard,
+            $rule -> new
         );
 
-        $rule->file->nonempty;
-        $rule->name("*.pm", "*.t", "*.pl");
+        $rule -> file -> nonempty;
+        $rule -> name("*.pm", "*.t", "*.pl");
 
-        my @files = $rule->in($source);
+        my @files = $rule -> in($source);
 
         return @files;
     }
