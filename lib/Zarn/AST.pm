d48e32b93d8abd21249768000cbd364d1a57ca85
htrgouvea <hi@heitorgouvea.me>
2024-01-03 16:17:54 -0300

    fix regex warnings from linter

--- lib/Zarn/AST.pm
+++ lib/Zarn/AST.pm
@@ -6,7 +6,7 @@ package Zarn::AST {
     use PPI::Document;
     
     our $VERSION = '0.01';
-    
+
     sub new {
         my ($self, $parameters) = @_;
         my ($file, $rules, @results);
@@ -29,11 +29,11 @@ package Zarn::AST {
                     my $category = $rule -> {category};
                     my $title    = $rule -> {name};
 
-                    if (grep {my $content = $_; scalar(grep {$content =~ m/$_/x} @sample)} $token -> content()) {
+                    if (grep {my $content = $_; scalar(grep {$content =~ m/$_/xms} @sample)} $token -> content()) {
                         my $next_element = $token -> snext_sibling;
 
                         # this is a draft source-to-sink function
-                        if (defined $next_element && ref $next_element && $next_element -> content() =~ /[\$\@\%](\w+)/x) {
+                        if (defined $next_element && ref $next_element && $next_element -> content() =~ /[\$\@\%](\w+)/xms) {
                             # perform taint analyis
                             my $var_token = $document -> find_first (
                                 sub { $_[1] -> isa("PPI::Token::Symbol") and $_[1] -> content eq "\$$1" }
