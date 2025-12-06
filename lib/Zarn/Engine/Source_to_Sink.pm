4a7c3613e436e5063782b97bb704af2277daae26
htrgouvea <hi@heitorgouvea.me>
2025-12-05 21:22:15 -0300

    more intuitive code structure

--- lib/Zarn/Engine/Source_to_Sink.pm
+++ lib/Zarn/Engine/Source_to_Sink.pm
@@ -7,7 +7,7 @@ package Zarn::Engine::Source_to_Sink {
     use PPI::Document;
     use Zarn::Engine::Taint_Analysis;
 
-    our $VERSION = '0.0.3';
+    our $VERSION = '0.0.4';
 
     sub new {
         my ($self, $parameters) = @_;
@@ -19,73 +19,70 @@ package Zarn::Engine::Source_to_Sink {
             'rules=s' => \$rules
         );
 
+        return 0 unless ($ast && $rules);
 
-        if ($ast && $rules) {
-            my @absence = grep { $_ -> {type} && $_ -> {type} eq 'absence' } $rules -> @*;
-            
-            for my $rule (@absence) {
+        my @absence = grep { $_ -> {type} && $_ -> {type} eq 'absence' } $rules -> @*;
+
+        for my $rule (@absence) {
+            my $category = $rule -> {category};
+            my $title    = $rule -> {name};
+            my $message  = $rule -> {message};
+
+            foreach my $token ($rule -> {sample} -> @*) {
+                next if ($ast -> content() =~ m/$token/xms);
+
+                push @results, {
+                    category       => $category,
+                    title          => $title,
+                    message        => $message,
+                    line_sink      => 'n/a',
+                    rowchar_sink   => 'n/a',
+                    line_source    => 'n/a',
+                    rowchar_source => 'n/a'
+                };
+            }
+        }
+
+        my @presence = grep { !($_ -> {type}) || $_ -> {type} eq 'presence' } $rules -> @*;
+
+        foreach my $token (@{$ast -> find('PPI::Token') || []}) {
+            foreach my $rule (@presence) {
+                my @sample   = $rule -> {sample} -> @*;
                 my $category = $rule -> {category};
                 my $title    = $rule -> {name};
                 my $message  = $rule -> {message};
 
-                foreach my $token ($rule -> {sample} -> @*) {
+                next unless any { my $content = $_; scalar(any { $content =~ m/$_/xms } @sample) } $token -> content();
 
-                    if ($ast -> content() !~ m/$token/xms ) {
-                        push @results, {
-                            category       => $category,
-                            title          => $title,
-                            message        => $message,
-                            line_sink      => 'n/a',
-                            rowchar_sink   => 'n/a',
-                            line_source    => 'n/a',
-                            rowchar_source => 'n/a'
-                        };
-                    }
-                }
+                my $next_element = $token -> snext_sibling;
+                next unless (defined $next_element && ref $next_element);
+
+                my $variable_name = $next_element -> content() =~ /[\$\@\%](\w+)/xms ? $1 : undef;
+                next unless $variable_name;
+
+                my $taint_analysis = Zarn::Engine::Taint_Analysis -> new ([
+                    '--ast'   => $ast,
+                    '--token' => $variable_name
+                ]);
+
+                next unless $taint_analysis;
+
+                my ($line_sink, $rowchar_sink) = @{$token -> location};
+                my ($line_source, $rowchar_source) = @{$taint_analysis};
+
+                push @results, {
+                    category       => $category,
+                    title          => $title,
+                    message        => $message,
+                    line_sink      => $line_sink,
+                    rowchar_sink   => $rowchar_sink,
+                    line_source    => $line_source,
+                    rowchar_source => $rowchar_source
+                };
             }
-
-            my @presence = grep { !($_ -> {type}) || $_ -> {type} eq 'presence' } $rules -> @*;
-
-            foreach my $token (@{$ast -> find('PPI::Token') || []}) {
-                foreach my $rule (@presence) {
-                    my @sample   = $rule -> {sample} -> @*;
-                    my $category = $rule -> {category};
-                    my $title    = $rule -> {name};
-                    my $message  = $rule -> {message};
-
-                    if (any { my $content = $_; scalar(any { $content =~ m/$_/xms } @sample) } $token -> content()) {
-                        my $next_element = $token -> snext_sibling;
-
-                        # this is a draft source-to-sink function
-                        if (defined $next_element && ref $next_element && $next_element -> content() =~ /[\$\@\%](\w+)/xms) {
-                            my $taint_analysis = Zarn::Engine::Taint_Analysis -> new ([
-                                '--ast'   => $ast,
-                                '--token' => $1
-                            ]);
-
-                            if ($taint_analysis) {
-                                my ($line_sink, $rowchar_sink) = @{$token -> location};
-                                my ($line_source, $rowchar_source) = @{$taint_analysis};
-
-                                push @results, {
-                                    category       => $category,
-                                    title          => $title,
-                                    message        => $message,
-                                    line_sink      => $line_sink,
-                                    rowchar_sink   => $rowchar_sink,
-                                    line_source    => $line_source,
-                                    rowchar_source => $rowchar_source
-                                };
-                            }
-                        }
-                    }
-                }
-            }
-
-            return @results;
         }
 
-        return 0;
+        return @results;
     }
 }
 
